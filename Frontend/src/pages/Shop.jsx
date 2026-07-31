0cebe8a8d416b6c7ce58504c805d1d09e589895e
Aishwarypathak2004 <yashpathak0054@gmail.com>
2026-07-31 09:19:14 +0530

    Initial Commit

--- Frontend/src/pages/Shop.jsx
+++ Frontend/src/pages/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import ProductCard from "../components/ProductCard";
+import ProductCard from "../components/productCard";
 import "../styles/Shop.css";
 
 const Shop = () => {
