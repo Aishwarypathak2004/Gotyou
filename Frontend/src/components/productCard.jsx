5c5c8e1217fe4a5dcf399091f13f8396abd73135
Aishwarypathak2004 <yashpathak0054@gmail.com>
2026-07-31 09:11:12 +0530

    Initial Commit

--- Frontend/src/components/productCard.jsx
+++ Frontend/src/components/productCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from "react-router-dom"
-import "../styles/productCard.css"
+import "../styles/productcard.css"
 
 const ProductCard = ({product}) => {
   return (
