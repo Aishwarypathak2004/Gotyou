70e1cbd6ecd84134642acca5155118ac57cfee3b
Aishwarypathak2004 <yashpathak0054@gmail.com>
2026-07-31 09:45:05 +0530

    Initial Commit finally..

--- backend/routes/authroutes.js
+++ backend/routes/authroutes.js
@@ -1,6 +1,6 @@
 const express=require ("express");
 const router=express.Router();
-const {registerUser,loginUser,verifyOTP,getUsers}=require("../controllers/authController");
+const {registerUser,loginUser,verifyOTP,getUsers}=require("../controllers/authcontroller");
 const protect=require("../middlewares/authmiddleware");
 const admin=require("../middlewares/adminmiddleware");
 
